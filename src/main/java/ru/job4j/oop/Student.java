0dbf9a0dcde2b6f8880502de6568689e90e029d5
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2021-07-31 08:40:35 +0700

    1.2. Вызов метода объекта [#166085 #183324]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -5,10 +5,21 @@ public class Student {
         System.out.println("I can sign a song : " + lyrics);
     }
 
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+
     public static void main(String[] args) {
         Student petya = new Student();
-        String song = "I believe, I can fly";
-        petya.music(song);
+
+        petya.music("Tra tra tra");
+        petya.music("Tra tra tra");
+        petya.music("Tra tra tra");
+
+        petya.song();
+        petya.song();
+        petya.song();
+
 
     }
 }
