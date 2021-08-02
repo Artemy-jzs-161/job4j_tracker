de5c63c5e90bd0ef06746c90c99405c9d8ba29f3
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2021-08-02 23:20:21 +0700

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int charge) {
+        this.load = charge;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery battery1 = new Battery(30);
+        System.out.println("Заряд первой батареи равен: " + battery1.load);
+        Battery battery2 = new Battery(100);
+        System.out.println("Заряд второй батареи равен: " + battery2.load);
+
+        battery1.exchange(battery2);
+        System.out.println("Псле зарядки размер  1 батареи равен: " + battery1.load);
+        System.out.println("Псле зарядки размер  2 батареи равен: " + battery2.load);
+
+    }
+
+}
