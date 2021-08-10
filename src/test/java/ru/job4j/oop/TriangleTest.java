1776fea0adf3bbe743a9ab57dc2375a4b113bf68
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2021-08-11 00:07:11 +0700

    2.2. Рефакторинг - Площадь треугольника. [#122642]

---
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TriangleTest {
+
+    @Test
+    public void checkTriangle() {
+        Point a = new Point(0, 6);
+        Point b = new Point(6, 0);
+        Point c = new Point(0, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double expected = 18.00;
+        double rsl = triangle.area();
+        Assert.assertEquals(expected, rsl, 0.01);
+
+    }
+}
