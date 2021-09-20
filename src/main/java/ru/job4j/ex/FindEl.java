91f0e77ce8340454f5e1ebaa8321b9b4cb66ee6f
Artemy-jzs-161 <borovik.artemy@yandex.ru>
2021-09-20 21:54:13 +0700

    9.2. Тесты вывода на консоль [#33585 #204678] fix

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl < 0) {
+            throw new ElementNotFoundException("Элемент не найден");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) throws ElementNotFoundException {
+        String[] valueEl = {"qaz", "wsx", "edc", "rfv"};
+        int a;
+        try {
+            a = indexOf(valueEl, "edca");
+            System.out.println(a);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
