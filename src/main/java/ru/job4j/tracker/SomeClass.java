574fc1a22648f90f6e41fc2016aceff5c778e0a4
Artemy <carapyz54@gmail.com>
2025-04-04 10:45:29 +0700

    3. Тестовое задание. Блок "Базовый синтаксис"

---
+++ src/main/java/ru/job4j/tracker/SomeClass.java
@@ -0,0 +1,7 @@
+package ru.job4j.tracker;
+
+public class SomeClass {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/ru/job4j/SomeTest.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+
+public class SomeTest {
+    @Test
+    void test() {
+
+    }
+
+}
